ac20d29dab982cb1e93c715713cd86ea068e0c56
efirst <eduardo.ha1991@gmail.com>
2023-11-21 07:46:12 -0500

    feat: update base routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {SearchComponent} from "./features/movie/infraestructure/ui/search/search.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: SearchComponent
+  },
+  {
+    path: 'movie/:id',
+    loadComponent: () => import('./features/movie/infraestructure/ui/search/search.component').then(c => c.SearchComponent)
+  }
+];
