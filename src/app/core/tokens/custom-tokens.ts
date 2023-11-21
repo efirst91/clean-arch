a73e33c0b4a7712662456ee05adb87050d19bc13
efirst <eduardo.ha1991@gmail.com>
2023-11-21 07:52:22 -0500

    feat: create custom tokens

---
+++ src/app/core/tokens/custom-tokens.ts
@@ -0,0 +1,5 @@
+import {InjectionToken} from "@angular/core";
+
+export const API_URL = new InjectionToken<string>('API_URL');
+export const IMG_URL = new InjectionToken<string>('IMG_URL');
+export const PROXY_URL = new InjectionToken<string>('PROXY_URL');
