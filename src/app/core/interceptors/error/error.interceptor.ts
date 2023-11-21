561e49e5f9e96c35127a46bbca78f6a5eba5cf41
efirst <eduardo.ha1991@gmail.com>
2023-11-21 08:16:21 -0500

    fix: update error interceptor config

--- src/app/core/interceptors/error/error.interceptor.ts
+++ src/app/core/interceptors/error/error.interceptor.ts
@@ -4,9 +4,11 @@ import {catchError,throwError} from "rxjs";
 export const errorInterceptor: HttpInterceptorFn = (req, next) => {
   return next(req).pipe(
     catchError((err: HttpErrorResponse) => {
-      debugger
+      // here we can do the error treatment according to http status error
+      // example if is 404 r 200, if you don't catch the error here then it is passed to the
+      // component, the error will be propagated using return  throwError(err);but if you don´t want
+      // to propagate the error then return EMPTY
       console.log('Error del interceptor ', err);
-      debugger
       return throwError(err);
     })
   );
