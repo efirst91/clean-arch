8f253405d41b216053d6a28086e8f5b6fca2fcca
efirst <eduardo.ha1991@gmail.com>
2023-11-21 08:13:04 -0500

    fix: update app config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,18 +3,19 @@ import {provideRouter} from '@angular/router';
 
 import {routes} from './app.routes';
 import {provideAnimations} from '@angular/platform-browser/animations';
-import {HTTP_INTERCEPTORS, provideHttpClient} from "@angular/common/http";
+import {provideHttpClient, withInterceptors} from "@angular/common/http";
 import {GlobalErrorHandlerService} from "./core/errors/global-error-handler.service";
-import {errorInterceptor} from "./core/interceptors/error/error.interceptor";
-import {API_URL, IMG_URL} from "./core/tokens/custom-tokens";
-import {environment} from "../environments/environment";
 import {MovieRepositoryService} from "./features/movie/infraestructure/respository/movie-repository.service";
+import {environment} from "../environments/environment";
+import {API_URL, IMG_URL} from "./core/tokens/custom-tokens";
+import {errorInterceptor} from "./core/interceptors/error/error.interceptor";
 import {headerInterceptor} from "./core/interceptors/header/header.interceptor";
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideHttpClient(),
+    provideHttpClient(
+      withInterceptors([errorInterceptor, headerInterceptor]),),
     provideAnimations(),
     {
       provide: IMG_URL,
@@ -31,16 +32,6 @@ export const appConfig: ApplicationConfig = {
     {
       provide: ErrorHandler,
       useClass: GlobalErrorHandlerService,
-    },
-    {
-      provide: HTTP_INTERCEPTORS,
-      useExisting: headerInterceptor,
-      multi: true
-    },
-    {
-      provide: HTTP_INTERCEPTORS,
-      useExisting: errorInterceptor,
-      multi: true
     }
   ]
 };
